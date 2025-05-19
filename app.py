7f070658f839c9606c201fe6297ac40b55ec0b99
MuhammedRafzan <166210649+MuhammedRafzan@users.noreply.github.com>
2025-05-20 00:48:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,4 +25,6 @@ def send_sms():
     return {'status': 'sent'}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 for local dev
+    app.run(host='0.0.0.0', port=port)
